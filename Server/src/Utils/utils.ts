97f1ad6b52fdb881db77a2ee517ce6bd202c20fc
CP Lepage <32472542+CPLepage@users.noreply.github.com>
2022-03-02 10:45:29 -0500

    Update utils.ts

--- Server/src/Utils/utils.ts
+++ Server/src/Utils/utils.ts
@@ -1,8 +1,7 @@
 import {Request} from "express";
 
 export function getAccessTokenFromRequest(request: Request): string{
-    if(request.query.accessToken)
-        return request.query.accessToken as string;
-
-    return request.headers.authorization as string
+    return request.query.accessToken ? 
+        request.query.accessToken as string : 
+        request.headers.authorization as string
 }
